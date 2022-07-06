3910fba22e07453e5dd4aa11ede3a13b138ca52a
Leo Kwan <leojkwan@gmail.com>
2022-07-06 16:15:43 -0400

    actually add sentry

--- pages/api/schedule/[scheduleId].ts
+++ pages/api/schedule/[scheduleId].ts
@@ -7,6 +7,9 @@ export default withApiAuthRequired(withSentry(async function showSchedule(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+
+  throw new Error("API throw error test")
+  return res.status(200)
   const { user } = getSession(req, res);
   const fetchRecentActivities = req.query?.fetchRecentActivities;
 
